d014742396c6bd2fd64c37af18fa14a43be2a44d
Vankayalapati Nagadurga <vvndurga1007@gmail.com>
2017-04-24 09:32:43 +0530

    added webpack bundler

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+    render() {
+        return(<div className="heading">
+            <h1>Hello World</h1>
+        </div>);
+    }
+}
