3cf1cd08d23d8d87fb34014a44cb4f887673b2a1
Vankayalapati Nagadurga <vvndurga1007@gmail.com>
2018-06-19 17:07:33 +0530

    added tests for App Component

--- test/AppSpec.js
+++ test/AppSpec.js
@@ -2,7 +2,7 @@ import App from "./../src/components/App";
 import Message from "./../src/components/Message";
 import React from "react";
 import { assert, expect } from "chai";
-import { mount } from "enzyme";
+import { shallow, mount } from "enzyme";
 
 //Shallow Rendering using Enzyme
 describe("App", () => {
@@ -18,4 +18,44 @@ describe("App", () => {
         const messageComponents = wrapper.find(Message);
         expect(messageComponents).to.have.length(1);
     });
+
+    it("should update state with input value", () => {
+        const wrapper = mount(<App />);
+
+        const heading = wrapper.find('.heading');
+        const inputBox = wrapper.find('input');
+
+        wrapper.ref('inputMessage').get(0).value = 'Hello';
+        inputBox.simulate("keyUp", { "keyCode": 13 });
+
+        expect(heading.exists()).to.be.true;
+        expect(inputBox.exists()).to.be.true;
+        expect(wrapper.state().messages).to.deep.equal(['Hello']);
+
+    });
+
+    it("should render Message Component", () => {
+        const messages = ["Hello", "Wold"];
+        const wrapper = mount(<App />);
+        wrapper.setState({"messages":  messages});
+
+        const messageDom = wrapper.find('Message');
+        const heading = messageDom.find('h1');
+        console.log(wrapper.debug());
+
+         expect(messageDom).to.have.length(2);
+         expect(heading.exists()).to.be.true;
+    });
+
+    it("should delete message when we click on delete button", () => {
+        const messages = ["Hello", "Wold"];
+        const wrapper = mount(<App/>);
+        wrapper.setState({"messages": messages});
+
+        const messageDom = wrapper.find('Message').at(0);
+        messageDom.simulate("click");
+
+        expect(wrapper.state().messages).to.deep.equal(['World']);
+    });
+
 });
